f833bcf6ef65d34de6d089d027495ab0fe8b00b4
Gregor Medoš <medos.gregor@gmail.com>
2024-08-15 14:36:54 +0200

    compile and execute C code

---
+++ 01_test.c
@@ -0,0 +1,11 @@
+//preprocessor header files
+#include <stdio.h>
+
+
+//main function
+int main()
+{
+    printf("Hello World!\n"); //C uses functions
+    
+    return 0; //exit status = SUCCESS
+}
